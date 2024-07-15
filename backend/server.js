b9145f25032229427db4e2923ce2b45182fed28d
jaykantariya123 <jaykantariya22@gmail.com>
2024-07-15 22:05:44 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
-import 'dotenv/config';
+import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-
 
 //middleware
 app.use(express.json());
@@ -21,10 +20,10 @@ connectDB();
 //api endpoint
 
 app.use("/api/food", foodRouter);
-app.use("/api/user",userRouter);
-app.use("/api/cart",cartRouter);
-app.use("/api/order",orderRouter);
-app.use("/images", express.static("/tmp/uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/images", express.static("tmp/uploads"));
 
 app.get("/", (req, res) => {
   res.send("API Working");
