ed056d230937e500a6275502b8f753fdc98a0830
jaykantariya123 <jaykantariya22@gmail.com>
2024-07-15 20:39:05 +0530

    image changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [food_list, setFoodList] = useState([]);
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-backend-ecru.vercel.app";
   const [token, setToken] = useState("");
 
   const addToCart = async (itemId) => {
