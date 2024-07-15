59c24782fad8f58e2974bd7106b3f68cb16b1340
jaykantariya123 <jaykantariya22@gmail.com>
2024-07-15 18:11:46 +0530

    third commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://jaykantariya22:food@cluster0.ot9lmw9.mongodb.net/food-del"
-    )
+    .connect(process.env.MONGO_URL)
     .then(() => console.log("DB Connected"));
 };
