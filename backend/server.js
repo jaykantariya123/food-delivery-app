3f1ce12345ad531d43cb3cefcc35667c83b02cd4
jaykantariya123 <jaykantariya22@gmail.com>
2024-07-15 21:55:56 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/food", foodRouter);
 app.use("/api/user",userRouter);
 app.use("/api/cart",cartRouter);
 app.use("/api/order",orderRouter);
-app.use("/images", express.static('uploads'));
+app.use("/images", express.static("/tmp/uploads"));
 
 app.get("/", (req, res) => {
   res.send("API Working");
